f1e7e740ccbc557b183b5fd501fffb0bc26c5168
Ronaldo-001 <ronaldop1705@gmail.com>
2025-04-02 23:23:55 +0000

    added work files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
